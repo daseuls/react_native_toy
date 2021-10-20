99d02f4395d169f2e219d97b4313cf37c22d5f8f
daseul <dsl@planfit.ai>
2021-10-20 14:53:54 +0900

    컴포넌트 구조 변경

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,27 +1,124 @@
-import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
-import Nav from '../Nav/Nav';
-import Todo from '../Todo/Todo';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Button,
+  TextInput,
+} from 'react-native';
 
 const Main = () => {
+  const [today, setToday] = useState(true);
+  const [currentTodo, setCurrentTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const inputRef = React.createRef();
+
+  const onSubmitInputValue = () => {
+    if (currentTodo === '') {
+      return;
+    }
+    setCurrentTodo('');
+    setTodoList([...todoList, currentTodo]);
+  };
+
+  const onChangeText = text => setCurrentTodo(text);
+
+  const handleToday = () => {
+    setToday(true);
+  };
+
+  const handleWeekly = () => {
+    setToday(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Nav />
-      <Todo />
-      <View style={styles.footer}></View>
+    <View style={styles.main}>
+      <View style={styles.nav}>
+        <TouchableOpacity onPress={handleToday}>
+          <Text style={styles.navMenu}>Today 📝</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleWeekly}>
+          <Text style={styles.navMenu}>Weekly 📅</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.todo}>
+        <View style={styles.todoInputContainer}>
+          <TextInput
+            ref={inputRef}
+            placeholder={
+              today
+                ? "Write your today's todolist"
+                : 'Write your weekly todolist'
+            }
+            returnKeyType="done"
+            style={styles.todoSubmitInput}
+            onChangeText={onChangeText}
+            onSubmitEditing={onSubmitInputValue}></TextInput>
+
+          {/* <Button title="입력" onPress={onSubmitInputValue}></Button> */}
+        </View>
+        <View style={styles.todoListsContainer}>
+          {todoList.map(todo => (
+            <View style={styles.todoListContainer}>
+              <Text style={styles.todoList}>{todo}</Text>
+              <Button title="삭제"></Button>
+            </View>
+          ))}
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
     backgroundColor: 'yellow',
   },
-
-  footer: {
-    flex: 0.3,
-    backgroundColor: 'pink',
+  todo: {
+    flex: 1,
+    backgroundColor: 'white',
+    // alignItems: 'center',
+  },
+  todoInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  todoSubmitInput: {
+    width: '70%',
+    padding: 7,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: 'grey',
+  },
+  todoListsContainer: {
+    marginTop: 20,
+    marginLeft: 50,
+  },
+  todoListContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  todoList: {
+    fontSize: 20,
+  },
+  nav: {
+    flex: 0.1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: 'skyblue',
+    marginTop: 50,
+    fontSize: 25,
+  },
+  navMenu: {
+    fontSize: 20,
+    fontWeight: '600',
   },
 });
 
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,33 +1,14 @@
-import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+// import React, {useState} from 'react';
+// import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
-const Nav = () => {
-  return (
-    <View style={styles.nav}>
-      <Text style={styles.navMenu}>Past</Text>
-      <Text style={styles.navMenu}>2021</Text>
-      <Text style={styles.navMenu}>Life</Text>
-      <Text style={styles.emoji}>📝</Text>
-    </View>
-  );
-};
+// const Nav = ({today, handleToday, handleWeekly}) => {
+//   return (
 
-const styles = StyleSheet.create({
-  nav: {
-    flex: 0.2,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    backgroundColor: 'skyblue',
-    marginTop: 50,
-    fontSize: 25,
-  },
-  emoji: {
-    fontSize: 30,
-  },
-  navMenu: {
-    fontSize: 20,
-  },
-});
+//   );
+// };
 
-export default Nav;
+// const styles = StyleSheet.create({
+
+// });
+
+// export default Nav;
